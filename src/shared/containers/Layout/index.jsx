78c20958e137932b653299321c4dcbdc7f110d35
Aleksy Vyaznikov <alekseyvyaznikov@gmail.com>
2018-02-15 23:36:51 +0300

    pixel fixed

--- src/shared/containers/Layout/index.jsx
+++ src/shared/containers/Layout/index.jsx
@@ -8,6 +8,20 @@ import style from './styles.styl';
 class Layout extends Component {
 	componentDidUpdate(prevProps, prevState) {
 		if (prevProps.location.pathname !== this.props.location.pathname) {
+			fbq('track', 'PageView');
+			!function (f, b, e, v, n, t, s) {
+				if (f.fbq) return; n = f.fbq = function () {
+					n.callMethod ?
+						n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+				};
+				if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+				n.queue = []; t = b.createElement(e); t.async = !0;
+				t.src = v; s = b.getElementsByTagName(e)[0];
+				s.parentNode.insertBefore(t, s)
+			}(window, document, 'script',
+				'https://connect.facebook.net/en_US/fbevents.js');
+			fbq('init', '947817732034649');
+			fbq('track', 'PageView');
 			!function (f, b, e, v, n, t, s) {
 				if (f.fbq) return; n = f.fbq = function () {
 					n.callMethod ?
@@ -20,20 +34,7 @@ class Layout extends Component {
 			}(window, document, 'script',
 				'https://connect.facebook.net/en_US/fbevents.js');
 			fbq('init', '210347599540942');
-			fbq('track', 'PageView');
-			!function (f, b, e, v, n, t, s) {
-				if (f.fbq) return; n = f.fbq = function () {
-					n.callMethod ?
-					n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-				};
-				if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
-				n.queue = []; t = b.createElement(e); t.async = !0;
-				t.src = v; s = b.getElementsByTagName(e)[0];
-				s.parentNode.insertBefore(t, s)
-			}(window, document, 'script',
-				'https://connect.facebook.net/en_US/fbevents.js');
-			fbq('init', '947817732034649');
-			fbq('track', 'PageView');
+
 		}
 	}
 	componentDidMount() {
