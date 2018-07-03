f81a7bf6842502a56d19bfe23108c0dab3a96227
Aleksy Vyaznikov <alekseyvyaznikov@gmail.com>
2018-07-03 10:44:39 +0300

    upadate package

--- tools/webpack/createWebpackConfig.js
+++ tools/webpack/createWebpackConfig.js
@@ -5,6 +5,7 @@ import fs from 'fs';
 import webpack from 'webpack';
 import dotenv from 'dotenv';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import BabelMinifyPlugin from 'babel-minify-webpack-plugin';
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
 import CaseSensitivePathsPlugin from 'case-sensitive-paths-webpack-plugin';
 import CircularDependencyPlugin from 'circular-dependency-plugin';
@@ -36,11 +37,13 @@ dotenv.config();
 process.traceDeprecation = true;
 const envParameters = Object.keys(process.env);
 // Fail early if our env vars arent set.
-const missingParameters = REQUIRED_ENV_VARS.filter(key => !envParameters.includes(key));
+const missingParameters = REQUIRED_ENV_VARS.filter(
+	key => !envParameters.includes(key)
+);
 if (missingParameters.length > 0) {
 	throw new Error(
 		`Missing environment parameters ${missingParameters.join(', ')}.\n` +
-			`Hint: Please provide a proper .env file`,
+			`Hint: Please provide a proper .env file`
 	);
 }
 
@@ -92,14 +95,16 @@ export default function createWebpackConfig(options) {
 		JSON.stringify(PKG_JSON),
 		CACHE_HASH_TYPE,
 		CACHE_DIGEST_TYPE,
-		CACHE_DIGEST_LENGTH,
+		CACHE_DIGEST_LENGTH
 	);
 
 	// different cache dir for different environments and targets
 	const CACHE_LOADER_DIRECTORY = path.resolve(
 		ROOT,
 		// $FlowIssue
-		`node_modules/.cache/loader-${CACHE_HASH}-${config.target}-${config.env}`,
+		`node_modules/.cache/loader-${CACHE_HASH}-${config.target}-${
+			config.env
+		}`
 	);
 
 	const browserTargets = {
@@ -112,7 +117,7 @@ export default function createWebpackConfig(options) {
 
 	const name = _IS_CLIENT_ ? 'client' : 'server';
 	const target = _IS_CLIENT_ ? 'web' : 'node';
-	const devtool = _IS_DEV_ ? 'cheap-module-source-map' : 'source-map';
+	const devtool = _IS_DEV_ ? 'cheap-module-inline-source-map' : false;
 
 	const cacheLoader = {
 		loader: 'cache-loader',
@@ -124,9 +129,11 @@ export default function createWebpackConfig(options) {
 		modules: true,
 		localIdentName: LOCAL_IDENT,
 		import: 3,
-		minimize: _IS_DEV_ ? false : {
-			discardComments: { removeAll: true },
-		},
+		minimize: _IS_DEV_
+			? false
+			: {
+					discardComments: { removeAll: true },
+			  },
 		sourceMap: _IS_DEV_,
 	};
 
@@ -188,8 +195,10 @@ export default function createWebpackConfig(options) {
 			// build/assets/*
 			path: _IS_SERVER_ ? SERVER_OUTPUT : CLIENT_OUTPUT,
 			libraryTarget: _IS_SERVER_ ? 'commonjs2' : 'var',
-			filename: _IS_DEV_ || _IS_SERVER_ ? '[name].js' : '[name]-[chunkhash].js',
-			chunkFilename: _IS_DEV_ || _IS_SERVER_ ? '[name].js' : '[name]-[chunkhash].js',
+			filename:
+				_IS_DEV_ || _IS_SERVER_ ? '[name].js' : '[name]-[chunkhash].js',
+			chunkFilename:
+				_IS_DEV_ || _IS_SERVER_ ? '[name].js' : '[name]-[chunkhash].js',
 			// Full URL in dev otherwise we expect our bundled output to be served from /assets/
 			publicPath: '/assets/',
 			// only dev
@@ -215,7 +224,7 @@ export default function createWebpackConfig(options) {
 					console: true,
 					__filename: true,
 					__dirname: true,
-				}
+			  }
 			: {
 					Buffer: false,
 					__dirname: false,
@@ -223,26 +232,44 @@ export default function createWebpackConfig(options) {
 					console: false,
 					global: true,
 					process: false,
-				},
+			  },
 		performance: _IS_DEV_
 			? false
 			: {
 					hints: 'warning',
-				},
+			  },
 
 		resolve: {
 			// look for files in the descendants of src/ then node_modules
-			modules: ['node_modules', SRC_DIR, path.resolve(ROOT, 'node_modules')],
+			modules: [
+				'node_modules',
+				SRC_DIR,
+				path.resolve(ROOT, 'node_modules'),
+			],
 			// Webpack will look for the following fields when searching for libraries
 			mainFields: _IS_CLIENT_
-				? ['browser:modern', 'browser:esnext', 'web:modern', 'browser', 'module', 'main']
-				: ['esnext:main', 'module:modern', 'main:modern', 'jsnext:main', 'module', 'main'],
+				? [
+						'browser:modern',
+						'browser:esnext',
+						'web:modern',
+						'browser',
+						'module',
+						'main',
+				  ]
+				: [
+						'esnext:main',
+						'module:modern',
+						'main:modern',
+						'jsnext:main',
+						'module',
+						'main',
+				  ],
 			descriptionFiles: ['package.json'],
 			// We want files with the following extensions...
 			extensions: ['.js', '.jsx', '.mjs', '.json', '.css', '.scss'],
 			alias: {
 				'babel-runtime': relativeResolve('babel-runtime/package.json'),
-				'components': path.resolve(SRC_DIR + '/shared/components'),
+				components: path.resolve(SRC_DIR + '/shared/components'),
 				pages: path.resolve(SRC_DIR + '/shared/pages'),
 				containers: path.resolve(SRC_DIR + '/shared/containers'),
 				config: path.resolve(SRC_DIR + '/shared/config'),
@@ -252,14 +279,19 @@ export default function createWebpackConfig(options) {
 		},
 		module: {
 			strictExportPresence: true,
-			
+
 			rules: [
 				{
 					test: JS_FILES,
-					use: ["source-map-loader"],
-					enforce: "pre",
+					use: ['source-map-loader'],
+					enforce: 'pre',
 					// these can be problematic loading sourcemaps that may/may not exist
-					exclude: [/apollo-/, /zen-observable-ts/, /react-apollo/, /intl-/],
+					exclude: [
+						/apollo-/,
+						/zen-observable-ts/,
+						/react-apollo/,
+						/intl-/,
+					],
 				},
 				// url loader for webfonts
 				// {
@@ -315,7 +347,9 @@ export default function createWebpackConfig(options) {
 						/\.png$/,
 					],
 					options: {
-						name: _IS_PROD_ ? 'file-[hash:base62:8].[ext]' : '[name].[ext]',
+						name: _IS_PROD_
+							? 'file-[hash:base62:8].[ext]'
+							: '[name].[ext]',
 						// dont emit a file for the server
 						emitFile: _IS_CLIENT_,
 					},
@@ -336,54 +370,54 @@ export default function createWebpackConfig(options) {
 					test: /\.css$/,
 					include: NODE_DIR,
 					exclude: SRC_DIR,
-					use: _IS_CLIENT_ ? 
-						[
-							MiniCssExtractPlugin.loader,
-							{
-								loader: 'css-loader',
-								options: {
-									modules: false,
-									localIdentName: LOCAL_IDENT,
-									import: 3,
-									minimize: !_IS_DEV_,
-									sourceMap: _IS_DEV_,
+					use: _IS_CLIENT_
+						? [
+								MiniCssExtractPlugin.loader,
+								{
+									loader: 'css-loader',
+									options: {
+										modules: false,
+										localIdentName: LOCAL_IDENT,
+										import: 3,
+										minimize: !_IS_DEV_,
+										sourceMap: _IS_DEV_,
+									},
 								},
-							},
-							{
-								loader: 'resolve-url-loader',
-							},
-						].filter(Boolean)
+								{
+									loader: 'resolve-url-loader',
+								},
+						  ].filter(Boolean)
 						: [
-							cacheLoader,
-							{
-								loader: 'css-loader/locals',
-								options: {
-									modules: false,
-									localIdentName: LOCAL_IDENT,
-									import: 3,
-									minimize: _IS_DEV_ ? false : true,
-									sourceMap: _IS_DEV_,
+								cacheLoader,
+								{
+									loader: 'css-loader/locals',
+									options: {
+										modules: false,
+										localIdentName: LOCAL_IDENT,
+										import: 3,
+										minimize: _IS_DEV_ ? false : true,
+										sourceMap: _IS_DEV_,
+									},
 								},
-							},
-							{
-								loader: 'resolve-url-loader',
-							},
-						].filter(Boolean),
+								{
+									loader: 'resolve-url-loader',
+								},
+						  ].filter(Boolean),
 				},
 				{
 					test: STYLE_FILES,
 					include: SRC_DIR,
 					exclude: /node_modules/,
 					use: _IS_CLIENT_
-						?  [
+						? [
 								MiniCssExtractPlugin.loader,
-									{
-										loader: 'css-loader',
-										options: cssLoaderOptions,
-									},
-									postCSSLoaderRule,
-									stylysLoaderRule,
-							].filter(Boolean)
+								{
+									loader: 'css-loader',
+									options: cssLoaderOptions,
+								},
+								postCSSLoaderRule,
+								stylysLoaderRule,
+						  ].filter(Boolean)
 						: [
 								{
 									loader: 'css-loader/locals',
@@ -391,31 +425,33 @@ export default function createWebpackConfig(options) {
 								},
 								postCSSLoaderRule,
 								stylysLoaderRule,
-							].filter(Boolean),
+						  ].filter(Boolean),
 				},
 			].filter(Boolean),
 		},
-		optimization: _IS_CLIENT_ ? {
-			splitChunks: {
-				chunks: 'async',
-				minSize: 30000,
-				maxAsyncRequests: 5,
-				maxInitialRequests: 3,
-				automaticNameDelimiter: '~',
-				minChunks: Infinity,
-				cacheGroups: {
-					vendors: {
-						test: /[\\/]node_modules[\\/]/,
-						priority: -10
+		optimization: _IS_CLIENT_
+			? {
+					splitChunks: {
+						chunks: 'async',
+						minSize: 30000,
+						maxAsyncRequests: 5,
+						maxInitialRequests: 3,
+						automaticNameDelimiter: '~',
+						minChunks: Infinity,
+						cacheGroups: {
+							vendors: {
+								test: /[\\/]node_modules[\\/]/,
+								priority: -10,
+							},
+							default: {
+								minChunks: 2,
+								priority: -20,
+								reuseExistingChunk: true,
+							},
+						},
 					},
-					default: {
-						minChunks: 2,
-						priority: -20,
-						reuseExistingChunk: true
-					}
-				}
-			}
-		} : {},
+			  }
+			: {},
 		plugins: [
 			new webpack.LoaderOptionsPlugin({
 				minimize: _IS_PROD_,
@@ -431,19 +467,21 @@ export default function createWebpackConfig(options) {
 				'process.env.API_URL': JSON.stringify(process.env.API_URL),
 				'process.env.TARGET': JSON.stringify(webpackTarget),
 			}),
-			
+
 			_IS_DEV_
 				? new WriteFilePlugin({
 						exitOnErrors: false,
 						log: true,
 						// required not to cache removed files
 						useHashIndex: false,
-					})
+				  })
 				: null,
 			_IS_CLIENT_
 				? new MiniCssExtractPlugin({
-						filename: _IS_DEV_ ? '[name].css' : '[name]-[contenthash:base62:8].css'
-					})
+						filename: _IS_DEV_
+							? '[name].css'
+							: '[name]-[contenthash:base62:8].css',
+				  })
 				: null,
 			// explicit named vendor chunk
 			// Extract Webpack bootstrap code with knowledge about chunks into separate cachable package.
@@ -459,34 +497,34 @@ export default function createWebpackConfig(options) {
 			new SpriteLoaderPlugin(),
 			_IS_DEV_ && _IS_CLIENT_
 				? new AutoDllPlugin({
-					context: ROOT,
-					filename: '[name].js',
-					entry: {
-						vendor: [
-							'react',
-							'react-dom',
-							'react-router-dom',
-							'redux',
-							'react-redux',
-							'redux-thunk',
-							'redux-logger',
-							'styled-components',
-							'react-helmet',
-							'serialize-javascript',
-							'prop-types',
-							'history',
-							'react-universal-component',
-						],
-					},
-				})
+						context: ROOT,
+						filename: '[name].js',
+						entry: {
+							vendor: [
+								'react',
+								'react-dom',
+								'react-router-dom',
+								'redux',
+								'react-redux',
+								'redux-thunk',
+								'redux-logger',
+								'styled-components',
+								'react-helmet',
+								'serialize-javascript',
+								'prop-types',
+								'history',
+								'react-universal-component',
+							],
+						},
+				  })
 				: null,
 			/**
-				* HappyPack Plugins are used as caching mechanisms to reduce the amount
-				* of time Webpack spends rebuilding, during your bundling during
-				* development.
-				* @see https://github.com/amireh/happypack for more info
-				* @type {String}   The HappyPack loader id
-			*/
+			 * HappyPack Plugins are used as caching mechanisms to reduce the amount
+			 * of time Webpack spends rebuilding, during your bundling during
+			 * development.
+			 * @see https://github.com/amireh/happypack for more info
+			 * @type {String}   The HappyPack loader id
+			 */
 			happyPackPlugin({
 				name: 'hp-js',
 				loaders: [
@@ -494,7 +532,7 @@ export default function createWebpackConfig(options) {
 						path: 'babel-loader',
 						query: {
 							babelrc: false,
-							sourceMaps: true,
+							sourceMap: _IS_DEV_,
 							comments: false,
 							cacheDirectory: _IS_DEV_,
 							compact: _IS_PROD_,
@@ -508,7 +546,9 @@ export default function createWebpackConfig(options) {
 											'transform-regenerator',
 											'transform-async-to-generator',
 										],
-										targets: _IS_CLIENT_ ? browserTargets : serverTargets,
+										targets: _IS_CLIENT_
+											? browserTargets
+											: serverTargets,
 									},
 								],
 								'react',
@@ -557,7 +597,6 @@ export default function createWebpackConfig(options) {
 					},
 				],
 			}),
-			
 
 			// Improve OS compatibility
 			// https://github.com/Urthen/case-sensitive-paths-webpack-plugin
@@ -569,7 +608,7 @@ export default function createWebpackConfig(options) {
 						exclude: /a\.js|node_modules/,
 						// show a warning when there is a circular dependency
 						failOnError: false,
-					})
+				  })
 				: null,
 			_IS_PROD_ && _IS_CLIENT_ ? new WebpackDigestHash() : null,
 			// Let the server side renderer know about our client side assets
@@ -585,48 +624,54 @@ export default function createWebpackConfig(options) {
 			// Analyse bundle in production
 			_IS_CLIENT_ && _IS_PROD_ && config.analyzeClientBundle
 				? new BundleAnalyzerPlugin.BundleAnalyzerPlugin({
-					analyzerMode: 'static',
-					defaultSizes: 'gzip',
-					logLevel: 'silent',
-					openAnalyzer: false,
-					reportFilename: 'report.html',
-				})
+						analyzerMode: 'static',
+						defaultSizes: 'gzip',
+						logLevel: 'silent',
+						openAnalyzer: false,
+						reportFilename: 'report.html',
+				  })
 				: null,
 			_IS_SERVER_ && _IS_PROD_ && config.analyzeServerBundle
 				? new BundleAnalyzerPlugin.BundleAnalyzerPlugin({
-					analyzerMode: 'static',
-					defaultSizes: 'parsed',
-					logLevel: 'silent',
-					openAnalyzer: false,
-					reportFilename: 'report.html',
-				})
+						analyzerMode: 'static',
+						defaultSizes: 'parsed',
+						logLevel: 'silent',
+						openAnalyzer: false,
+						reportFilename: 'report.html',
+				  })
 				: null,
 
 			_IS_SERVER_
 				? new webpack.BannerPlugin({
-					banner: 'require("source-map-support").install();',
-					raw: true,
-					entryOnly: false,
-				})
+						banner: 'require("source-map-support").install();',
+						raw: true,
+						entryOnly: false,
+				  })
 				: null,
-			_IS_SERVER_ ? new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }) : null,
-			_IS_PROD_ && _IS_CLIENT_ ? new BabelMinifyPlugin({}, { comments: false }) : null,
+			_IS_SERVER_
+				? new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 })
+				: null,
+			_IS_PROD_ && _IS_CLIENT_
+				? new BabelMinifyPlugin({}, { comments: false })
+				: null,
 			_IS_PROD_ && _IS_SERVER_
 				? new BabelMinifyPlugin(
-					{
-						booleans: false,
-						deadcode: true,
-						flipComparisons: false,
-						mangle: false,
-						mergeVars: false,
-					},
-					{ comments: false },
-				)
+						{
+							booleans: false,
+							deadcode: true,
+							flipComparisons: false,
+							mangle: false,
+							mergeVars: false,
+						},
+						{ comments: false }
+				  )
 				: null,
 
 			_IS_PROD_ ? new webpack.optimize.ModuleConcatenationPlugin() : null,
 			_IS_DEV_ ? new webpack.NoEmitOnErrorsPlugin() : null,
-			_IS_CLIENT_ && _IS_DEV_ ? new webpack.HotModuleReplacementPlugin() : null,
+			_IS_CLIENT_ && _IS_DEV_
+				? new webpack.HotModuleReplacementPlugin()
+				: null,
 		].filter(Boolean),
 	};
 }
